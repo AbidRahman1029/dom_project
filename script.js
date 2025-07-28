964a44ba0489f462b3bd04247f8a5761b1b782dd
rahmanabid465 <rahmanabid465@gmail.com>
2025-07-28 10:30:46 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Add your code to this file
+let learn = document.querySelector("h2")
+learn.style.textAlign = "center";
+
+let imagesarray = document.getElementsByClassName("pokemon_images")
+for (let i = 0; i < imagesarray.length; i++)
+{
+    imagesarray[i].style.border = "dotted";
+    imagesarray[i].style.borderColor = "yellow";
+}
+let image = document.querySelector("img")
+image.style.border = "dotted";
+image.style.borderColor = "yellow";
+
+let back = document.querySelector("body")
+back.style.backgroundColor = "blue";
